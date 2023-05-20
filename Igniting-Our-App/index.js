80a8a1540bd2b2e42cbb6b87e0fd7e2d3e04cd68
poorvaditya behre <aditya.behre@gmail.com>
2023-05-20 23:02:57 +0530

    added Ignite our app with parcel

---
+++ Igniting-Our-App/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
+  "h1",
+  { id: "title" },
+  "Heading 1 from parcel"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  { id: "title" },
+  "heading 2 from parcel"
+);
+
+const container = React.createElement("div", { id: "contatiner" }, [
+  heading1,
+  heading2,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);

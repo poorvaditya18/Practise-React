226fa6a46841b6af6a64f53f0c69c9330cefdfd0
poorvaditya behre <aditya.behre@gmail.com>
2023-05-27 12:52:52 +0530

    added different ways how to pass props

--- food-villa-app/App.js
+++ food-villa-app/App.js
@@ -48,39 +48,612 @@ const HeaderComponent = () => {
 
 const restaurantList = [
   {
-    name: "BurgerKing",
-    image:
-      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/e33e1d3ba7d6b2bb0d45e1001b731fcf",
-    cusines: ["Burger", "American"],
-    rating: "4.2",
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "247979",
+      name: "Roll Xpress",
+      uuid: "da77c4c3-5f27-4a58-8e88-d24775fd9965",
+      city: "6",
+      area: "Hinjawadi",
+      totalRatingsString: "1000+ ratings",
+      cloudinaryImageId: "fft5egyarxvjacrf8l1u",
+      cuisines: ["Snacks", "Beverages"],
+      tags: [],
+      costForTwo: 20000,
+      costForTwoString: "₹200 FOR TWO",
+      deliveryTime: 29,
+      minDeliveryTime: 29,
+      maxDeliveryTime: 29,
+      slaString: "29 MINS",
+      lastMileTravel: 2.299999952316284,
+      slugs: {
+        restaurant: "rolls-express-hinjewadi-hinjewadi",
+        city: "pune",
+      },
+      cityState: "6",
+      address: "Hinjewadi road near pakhare wasti wakad  pune pcmc 25",
+      locality: "Pakhare Wasti",
+      parentId: 170853,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3300,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3300,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3300",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=6578742~p=1~eid=00000188-5bdf-5e3b-753b-0db8004e0135",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "2.2 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "",
+        discountTag: "FLAT DEAL",
+        headerTypeV2: 0,
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        exclusiveOfferMessage: "",
+      },
+      sla: {
+        restaurantId: "247979",
+        deliveryTime: 29,
+        minDeliveryTime: 29,
+        maxDeliveryTime: 29,
+        lastMileTravel: 2.299999952316284,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "4.0",
+      totalRatings: 1000,
+      new: false,
+    },
+    subtype: "basic",
   },
-  
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "237669",
+      name: "McDonald's",
+      uuid: "c21261d3-1815-45dc-91a6-b6332ae0eaec",
+      city: "6",
+      area: "Hinjawadi",
+      totalRatingsString: "1000+ ratings",
+      cloudinaryImageId: "ee5f8e06b300efc07c9fe3f4df40dfc4",
+      cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+      tags: [],
+      costForTwo: 40000,
+      costForTwoString: "₹400 FOR TWO",
+      deliveryTime: 16,
+      minDeliveryTime: 16,
+      maxDeliveryTime: 16,
+      slaString: "16 MINS",
+      lastMileTravel: 0.699999988079071,
+      slugs: {
+        restaurant: "mcdonalds-blue-ridge-township-hinjewadi",
+        city: "pune",
+      },
+      cityState: "6",
+      address:
+        "McDonalds Family Restaurants Blue Ridge Township, NOB Bldg. Unit survey No. 119( Part) Hinjawadi phase 1 Pune 411057",
+      locality: "Blue Ridge Township",
+      parentId: 630,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3300,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3300,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3300",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "0.6 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "FREE ITEM",
+        subHeader: "",
+        discountTag: "",
+        headerTypeV2: 0,
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        exclusiveOfferMessage: "",
+      },
+      sla: {
+        restaurantId: "237669",
+        deliveryTime: 16,
+        minDeliveryTime: 16,
+        maxDeliveryTime: 16,
+        lastMileTravel: 0.699999988079071,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.3",
+      totalRatings: 1000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "3414",
+      name: "Aroma's Hyderabad House",
+      uuid: "1710a820-6e71-4131-9d06-bc615f75c853",
+      city: "6",
+      area: "Hinjawadi",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "cmiib4dln20zbr3xmpqk",
+      cuisines: ["Biryani", "Mughlai", "North Indian", "Chinese"],
+      tags: [],
+      costForTwo: 50000,
+      costForTwoString: "₹500 FOR TWO",
+      deliveryTime: 28,
+      minDeliveryTime: 28,
+      maxDeliveryTime: 28,
+      slaString: "28 MINS",
+      lastMileTravel: 1.5,
+      slugs: {
+        restaurant: "hyderabad-house-phase-1-hinjewadi",
+        city: "pune",
+      },
+      cityState: "6",
+      address:
+        "KPIT Cummins, Opposite Krishna Petrol Pump, Marunji Road, Hinjewadi, Pune",
+      locality: "Marunji Road",
+      parentId: 487,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3300,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3300,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3300",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "1.5 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+        discountTag: "",
+        headerTypeV2: 0,
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        exclusiveOfferMessage: "",
+      },
+      sla: {
+        restaurantId: "3414",
+        deliveryTime: 28,
+        minDeliveryTime: 28,
+        maxDeliveryTime: 28,
+        lastMileTravel: 1.5,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.1",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "5880",
+      name: "The Chinese Wok",
+      uuid: "bbacc617-6d0b-4af7-91ac-f297d1d97944",
+      city: "6",
+      area: "Om Heights",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "yenne8szjvjhjzbq0rj0",
+      cuisines: ["Chinese", "Thai", "Tibetan", "Pan-Asian"],
+      tags: [],
+      costForTwo: 45000,
+      costForTwoString: "₹450 FOR TWO",
+      deliveryTime: 35,
+      minDeliveryTime: 35,
+      maxDeliveryTime: 35,
+      slaString: "35 MINS",
+      lastMileTravel: 2.5999999046325684,
+      slugs: {
+        restaurant: "the-chinese-wok-opposite-shell-petrol-pump-hinjewadi",
+        city: "pune",
+      },
+      cityState: "6",
+      address:
+        "Om Heights, Opposite Shell Petrol Pump, Hinjewadi Main Road, Hinjewadi, Pune",
+      locality: "Hinjewadi",
+      parentId: 1001,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3300,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3300,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3300",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=6673484~p=31~eid=00000188-5bdf-5e3b-753b-0dc2004e1f24",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "2.5 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "ABOVE ₹800",
+        discountTag: "FLAT DEAL",
+        headerTypeV2: 0,
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        exclusiveOfferMessage: "",
+      },
+      sla: {
+        restaurantId: "5880",
+        deliveryTime: 35,
+        minDeliveryTime: 35,
+        maxDeliveryTime: 35,
+        lastMileTravel: 2.5999999046325684,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "4.1",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "441447",
+      name: "Burger King",
+      uuid: "a55ae3be-c0fc-47e4-b744-87b7b9fe4bda",
+      city: "6",
+      area: "Hinjewadi",
+      totalRatingsString: "5000+ ratings",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      cuisines: ["Burgers", "American"],
+      tags: [],
+      costForTwo: 35000,
+      costForTwoString: "₹350 FOR TWO",
+      deliveryTime: 21,
+      minDeliveryTime: 21,
+      maxDeliveryTime: 21,
+      slaString: "21 MINS",
+      lastMileTravel: 1.600000023841858,
+      slugs: {
+        restaurant: "burger-king-hinjewadi-hinjewadi",
+        city: "pune",
+      },
+      cityState: "6",
+      address:
+        "BURGER KING INDIA LIMITED , UNIT NO -18, SURATWALA, MARK PLAZZO, S. NO -27/1,27/5(P),28/B/1,2,3,4, HINJEWADI, PUNE 411057, HADASPAR, PUNE PMC&Rural, Maharashtra -411057",
+      locality: "Suratwala",
+      parentId: 166,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      chain: [],
+      feeDetails: {
+        fees: [
+          {
+            name: "distance",
+            fee: 3300,
+            message: "",
+          },
+          {
+            name: "time",
+            fee: 0,
+            message: "",
+          },
+          {
+            name: "special",
+            fee: 0,
+            message: "",
+          },
+        ],
+        totalFees: 3300,
+        message: "",
+        title: "Delivery Charge",
+        amount: "3300",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "1.6 kms",
+      hasSurge: false,
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+        discountTag: "",
+        headerTypeV2: 0,
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        exclusiveOfferMessage: "",
+      },
+      sla: {
+        restaurantId: "441447",
+        deliveryTime: 21,
+        minDeliveryTime: 21,
+        maxDeliveryTime: 21,
+        lastMileTravel: 1.600000023841858,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.0",
+      totalRatings: 5000,
+      new: false,
+    },
+    subtype: "basic",
+  },
 ];
 
-const RestaurantCard = () => {
+const RestaurantCard = ({
+  name,
+  cuisines,
+  cloudinaryImageId,
+  lastMileTravelString,
+}) => {
+  // object destructuring -->
+  // const { name, cuisines, cloudinaryImageId, lastMileTravelString } =
+  //   restaurant.data;
   return (
     <div className="card">
-      <img src={burgerKing.image}></img>
-      <h2>{burgerKing.name}</h2>
-      <h3>{burgerKing.cusines.join(",")}</h3>
-      <h4>{burgerKing.rating} stars</h4>
+      <img
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          cloudinaryImageId
+        }
+      ></img>
+      <h2>{name}</h2>
+      {/* <h3>{cuisines.join(",")}</h3> */}
+      {/* <h4>{lastMileTravelString} ETA</h4> */}
     </div>
   );
 };
 
+// props
 const Body = () => {
   return (
     <div className="restaurant-list">
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
-      <RestaurantCard />
+      <RestaurantCard
+        name={restaurantList[0].data.name}
+        cloudinaryImageId={restaurantList[0].data.cloudinaryImageId}
+      />
+      <RestaurantCard
+        name={restaurantList[1].data.name}
+        cloudinaryImageId={restaurantList[1].data.cloudinaryImageId}
+      />
+      <RestaurantCard
+        name={restaurantList[2].data.name}
+        cloudinaryImageId={restaurantList[2].data.cloudinaryImageId}
+      />
+      <RestaurantCard
+        name={restaurantList[3].data.name}
+        cloudinaryImageId={restaurantList[3].data.cloudinaryImageId}
+      />
+      {/* Instead of passing them individually you can pass via spreadOf operator */}
+      <RestaurantCard {...restaurantList[4].data} />
     </div>
   );
 };
